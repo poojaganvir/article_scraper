75b071ab28dbbbfa88d44b5543d9ff547b506e5c
pooja <pooja@zeromileedutech.com>
2017-02-09 15:12:39 +0530

    Created functionn to get all urls

--- another.js
+++ another.js
@@ -1,1 +1,22 @@
-// This will be the second script
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'pooja_database'
+});
+
+var getArticles = function() {
+	var sql = "SELECT * FROM links LIMIT 5";
+	connection.query(sql, function(error, results, fields) {
+		if (error) {
+			console.log(error);
+			return;
+		}
+
+		console.log(results);
+	});
+}; 
+
+getArticles();
